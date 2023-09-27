603a0f4e5bde8de80f1057d765ddfbdcfa2907af
林淡 <chenyuge.gcy@alibaba-inc.com>
2023-09-27 21:14:31 +0800

    feat: 完成汉服科普页面

--- src/pages/population-list/population-list.ts
+++ src/pages/population-list/population-list.ts
@@ -1,29 +1,40 @@
-import { aPage } from '@ali/mor-core';
-import { post } from '../../services';
+import { aPage } from '@ali/mor-core'
+import { post } from '../../services'
 
 aPage({
   data: {
-    types: [{
-      label: '全部',
-      value: 'all'
-    }, {
-      label: '上衣下裳',
-      value: '1'
-    }, {
-      label: '上下连裳',
-      value: '2'
-    }, {
-      label: '上下通裁',
-      value: '3'
-    }],
-    articles: []
+    types: [
+      {
+        label: '全部',
+        value: 'all',
+      },
+      {
+        label: '上衣下裳',
+        value: '1',
+      },
+      {
+        label: '上下连裳',
+        value: '2',
+      },
+      {
+        label: '上下通裁',
+        value: '3',
+      },
+    ],
+    dataSource: {},
   },
   onLoad() {
-    post('/sports/getArticles').then(articles => {
-      this.setData({articles});
-    });
+    post('/population/list').then((dataSource) => {
+      this.setData({ dataSource })
+    })
   },
-  onTabsChange(event:any) {
-    console.log(`Change tab, tab-panel value is ${event.detail.value}.`);
+  onTap(info: any) {
+    console.log('=====  onTap', info)
+    my.navigateTo({
+      url: `/pages/article-detail/article-detail?id=${info.key}`,
+      fail: () => {
+        my.navigateTo({ url: '/pages/pageNotFound/pageNotFound' })
+      },
+    })
   },
-});
+})
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,13 +1,14 @@
-import activities from '../../mock/request/home/getActivities';
-import articles from '../../mock/request/home/getArticles';
-import sportsArticles from '../../mock/request/sports/getArticles';
+import activities from '../../mock/request/home/getActivities'
+import articles from '../../mock/request/home/getArticles'
+import sportsArticles from '../../mock/request/sports/getArticles'
+import populationList from '../../mock/request/population/list'
 
-const map:any = {
+const map: any = {
   '/home/getActivities': activities,
   '/home/getArticles': articles,
   '/sports/getArticles': sportsArticles,
+  '/population/list': populationList,
 }
-export const post = (url:string, params?: any) => {
-
-  return Promise.resolve(map[url]?.data);
+export const post = (url: string, params?: any) => {
+  return Promise.resolve(map[url]?.data)
 }
