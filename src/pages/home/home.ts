bac09b7ea48bc9f7f74d944ff49249fe99fa8fa3
林淡 <chenyuge.gcy@alibaba-inc.com>
2023-11-01 18:43:25 +0800

    feat: 补充首页交互

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,27 +1,26 @@
 import { aPage } from '@ali/mor-core'
-import { post, get } from '../../services'
+import { get } from '../../services'
 
 import dayjs from 'dayjs'
-
-// 获取全局 app 实例
-const app = getApp()
 
 aPage({
   data: {
     swiperList: [],
+    swiperData: [],
     activityList: [],
     recommendArticles: [],
   },
   onLoad() {
     // 获取广告位图片
     get('/home/getBannerImgs').then((data) => {
-      this.setData({ swiperList: data.map((o: any) => o.url) })
+      this.setData({ swiperList: data.map((o: any) => o.url), swiperData: data })
     })
 
     // 获取近期活动
     get('/home/getActivities', {}).then((data) => {
       this.setData({
         activityList: data.map((o: any) => ({
+          ...o,
           title: o.name,
           desc: o.detail.replace('<p>', '').replace('</p>', ''),
           url: o.cover,
@@ -34,10 +33,26 @@ aPage({
 
     // 获取推荐文章
     get('/home/getArticles').then((recommendArticles) => {
-      console.log('=====  data', recommendArticles)
       this.setData({ recommendArticles })
     })
   },
+  onTapSwiper(e: any) {
+    const { jump, jump_link }: any = this.data.swiperData[e.detail.index]
+    console.log('=====  this.data.swiperList[index]', this.data.swiperData[e.detail.index])
+    switch (jump) {
+      case '1':
+        my.navigateTo({
+          url: `/pages${jump_link}`,
+          fail: () => {
+            my.navigateTo({ url: '/pages/pageNotFound/pageNotFound' })
+          },
+        })
+        break
+      default:
+        break
+    }
+  },
+  // 跳转页面的统一函数
   jump2Page(e: any) {
     const path = e.currentTarget.dataset.path
     my.navigateTo({
@@ -45,6 +60,20 @@ aPage({
       fail: () => {
         my.navigateTo({ url: '/pages/pageNotFound/pageNotFound' })
       },
+    })
+  },
+  // 跳转到活动详情页面
+  jump2ActivityDetailPage(e: any) {
+    const { _id } = e.currentTarget.dataset.item
+    my.navigateTo({
+      url: `/pages/activity/detail?activityId=${_id}`,
+    })
+  },
+  // 跳转到文章详情页面
+  jump2ArticleDetailPage(e: any) {
+    const { _id } = e.currentTarget.dataset.item
+    my.navigateTo({
+      url: `/pages/article/detail?articleId=${_id}`,
     })
   },
 })
