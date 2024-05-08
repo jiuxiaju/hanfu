602a5a6c7f2c3a2b8a49766afdea1b55b68a7322
林淡 <chenyuge.gcy@alibaba-inc.com>
2024-05-08 13:56:30 +0800

    feat: 搜索页面添加1

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,3 +1,39 @@
 import { aPage } from '@ali/mor-core'
 
-aPage({})
+aPage({
+  data: {
+    image: 'https://tdesign.gtimg.com/mobile/demos/empty1.png',
+    types: [
+      {
+        label: '综合',
+        value: 'all',
+        infoList: [],
+      },
+      {
+        label: '实体店',
+        value: '1',
+        infoList: [],
+      },
+      {
+        label: '网店',
+        value: '2',
+        infoList: [],
+      },
+      {
+        label: '科普',
+        value: '3',
+        infoList: [],
+      },
+      {
+        label: '活动',
+        value: '4',
+        infoList: [],
+      },
+      {
+        label: '文章',
+        value: '5',
+        infoList: [],
+      },
+    ],
+  },
+})
